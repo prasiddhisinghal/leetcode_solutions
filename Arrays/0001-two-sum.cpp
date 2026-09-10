acf43b389baf2ac9557b7a83845cdbd98ad30d78
Prasiddhi Singhal <prasiddhi.singhal16@gmail.com>
2026-09-11 01:50:19 +0530

    Optimize LeetCode 0001 - Two Sum

--- Arrays/0001-two-sum.cpp
+++ Arrays/0001-two-sum.cpp
@@ -30,3 +30,24 @@ public:
         return {};
     }
 };
+
+// optimised solution using hashmap //
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int needed = target - nums[i];
+
+            if (seen.find(needed) != seen.end()) {
+                return {seen[needed], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
